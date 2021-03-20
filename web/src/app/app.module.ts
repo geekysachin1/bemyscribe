c7272ea8e6a651a2e57e5556bdd1724a9ac946d4
Sreelekshmy S <sreelu28186@gmail.com>
2021-03-20 16:10:38 +0530

    Implemented localization of home page.

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,15 +3,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-    providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+    imports: [
+      BrowserModule,
+      SharedModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,
+      HttpClientModule,
+      TranslateModule.forRoot({
+        loader: {
+            provide: TranslateLoader,
+            useFactory: (createTranslateLoader),
+            deps: [HttpClient]
+        }
+    })],
+    providers: [
+      { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule { }
